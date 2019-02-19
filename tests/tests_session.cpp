2fe0a7daea6ee751f431d2420219ba220aba4095
Fábio da Silva Santana <fabio.ssantana.dev@gmail.com>
2019-02-19 11:16:11 -0300

    change format to LLVM

--- tests/tests_session.cpp
+++ tests/tests_session.cpp
@@ -1,36 +1,32 @@
 #include "catch2/catch.hpp"
 
-
 #include "session.h"
 
-#include <memory> //unique_ptr
+#include <memory>
 
-TEST_CASE("Try login", "[session]")
-{
-    auto session = std::make_unique<FTP::Session>();
+TEST_CASE("Try login", "[session]") {
+  auto session = std::make_unique<FTP::Session>();
 
-    REQUIRE(session->login("teste", "36145544"));
-    REQUIRE(session->isLogged());
-    REQUIRE(session->getToken() != nullptr);
+  REQUIRE(session->login("teste", "36145544"));
+  REQUIRE(session->isLogged());
+  REQUIRE(session->getToken() != nullptr);
 }
 
-TEST_CASE("Try login wrong data", "[session]")
-{
-    auto session = std::make_unique<FTP::Session>();
+TEST_CASE("Try login wrong data", "[session]") {
+  auto session = std::make_unique<FTP::Session>();
 
-    REQUIRE_FALSE(session->login("teste", "3614554"));
-    REQUIRE_FALSE(session->isLogged());
-    REQUIRE_FALSE(session->getToken() != nullptr);
+  REQUIRE_FALSE(session->login("teste", "3614554"));
+  REQUIRE_FALSE(session->isLogged());
+  REQUIRE_FALSE(session->getToken() != nullptr);
 }
 
-TEST_CASE("Try logout", "[session]")
-{
-    auto session = std::make_unique<FTP::Session>();
+TEST_CASE("Try logout", "[session]") {
+  auto session = std::make_unique<FTP::Session>();
 
-    REQUIRE(session->login("teste", "36145544"));
-    REQUIRE(session->isLogged());
-    REQUIRE(session->getToken() != nullptr);
-    session->logout();
-    REQUIRE_FALSE(session->isLogged());
-    REQUIRE_FALSE(session->getToken() != nullptr);
+  REQUIRE(session->login("teste", "36145544"));
+  REQUIRE(session->isLogged());
+  REQUIRE(session->getToken() != nullptr);
+  session->logout();
+  REQUIRE_FALSE(session->isLogged());
+  REQUIRE_FALSE(session->getToken() != nullptr);
 }
